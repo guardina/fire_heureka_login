c0445d9089d8f77c88c00298afe0614f5a7f0aa1
guardina <guardinialex@gmail.com>
2024-11-12 12:49:44 +0100

    Update table creation

--- sql/create_user_credentials.sql
+++ sql/create_user_credentials.sql
@@ -5,10 +5,12 @@ GRANT ALL PRIVILEGES ON fire_heureka_credentials.* TO 'alex'@'localhost';
 USE fire_heureka_credentials;
 
 CREATE TABLE IF NOT EXISTS user_credentials (
-    id INT AUTO_INCREMENT PRIMARY KEY,
-    username VARCHAR(255) NOT NULL UNIQUE,
-    password VARCHAR(255) NOT NULL         
+      id                    INT                 AUTO_INCREMENT          PRIMARY KEY
+    , username              VARCHAR(255)        NOT NULL                UNIQUE
+    , password              VARCHAR(255)        NOT NULL
+    , auth_token            VARCHAR(255)        NOT NULL                UNIQUE
+    , refresh_token         VARCHAR(255)        NOT NULL
+    , installation_id       VARCHAR(255)        NOT NULL
 );
 
 INSERT INTO user_credentials(username, password) VALUES('mock_user', 'mock_pw');
-
