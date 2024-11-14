9e9aa64b64a410cae890a9d0a9aa1a0af56b9b28
guardina <guardinialex@gmail.com>
2024-11-14 14:23:17 +0100

    Add update of tokens

--- app.py
+++ app.py
@@ -42,11 +42,10 @@ def login():
         username = request.form['username']
         password = request.form['password']
 
-        #hash_pw = hash_password(password)
-
         conn = get_db_connection()
         cursor = conn.cursor(dictionary=True)
 
+        #hash_pw = hash_password(password)
         #cursor.execute("INSERT INTO user_credentials(username, password) VALUES (%s, %s)", ("mock_user", hash_pw))
         #conn.commit()
         
@@ -65,7 +64,7 @@ def login():
             if not user_token:
                 return heureka_authorize()
             else:
-                return redirect(url_for('change_password'))
+                return redirect(url_for('heureka_api'))
         else:
             flash("Wrong password!")
             return render_template('login.html')
@@ -105,24 +104,12 @@ def redirected_page():
             installation_id = jwt.decode(access_token, options={"verify_signature": False}).get('sub')
             expiration_time = token_data.get('expires_in')
 
-            token_expiry = datetime.now() + timedelta(seconds=expiration_time)
+            save_token(access_token, refresh_token, expiration_time, 'insert')
 
             conn = get_db_connection()
             cursor = conn.cursor(dictionary=True)
 
             user_id = session.get('user_id')
-
-            cursor.execute("""
-            INSERT INTO user_tokens (user_id, access_token, refresh_token, token_expiry) 
-            VALUES (%s, %s, %s, %s)
-            ON DUPLICATE KEY UPDATE 
-                access_token = VALUES(access_token),
-                refresh_token = VALUES(refresh_token),
-                token_expiry = VALUES(token_expiry),
-                updated_at = CURRENT_TIMESTAMP
-            """
-            , (user_id, access_token, refresh_token, token_expiry))
-            conn.commit()
 
             cursor.execute("""
             UPDATE user_credentials SET installation_id = %s WHERE id = %s
@@ -141,11 +128,25 @@ def redirected_page():
 @app.route('/change_password', methods=['GET', 'POST'])
 def change_password():
     if request.method == 'POST':
+        user_id = session.get('user_id')
         password = request.form['password']
         password_conf = request.form['password_conf']
 
         if (password == password_conf):
-            print("password changed")
+            conn = get_db_connection()
+            cursor = conn.cursor(dictionary=True)
+
+            hpassword = hash_password(password)
+
+            cursor.execute("""
+            UPDATE user_credentials
+            SET password = %s
+            WHERE id = %s
+            """, (hpassword, user_id))
+            conn.commit()
+
+            return redirect(url_for('heureka_api'))
+
     return render_template('change_password.html')
 
 
@@ -157,14 +158,17 @@ def heureka_api():
     if not access_token:
         return jsonify({"error": "Could not obtain access token"}), 401
 
-    headers = {'Authorization': f'Bearer {access_token}'}
-    response = requests.get("https://api.example.com/protected", headers=headers)
+    #headers = {'Authorization': f'Bearer {access_token}'}
+    #response = requests.get("https://api.example.com/protected", headers=headers)
 
+    return render_template('heureka_connection.html')
+    
+    '''
     if response.status_code == 200:
         return jsonify(response.json())
     else:
         return jsonify({"error": "Failed to access the API", "details": response.json()}), response.status_code
-
+    '''
 
 
 
@@ -172,7 +176,20 @@ def heureka_api():
 ##############################################  TOKEN FUNCTIONS  ##############################################
 
 def get_new_access_token():
-    refresh_token = mySession.get('refresh_token')
+    user_id = session.get('user_id')
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT user_tokens.refresh_token
+    FROM user_credentials
+    JOIN user_tokens ON user_credentials.id = user_tokens.user_id
+    WHERE user_credentials.id = %s
+    """, (user_id,))
+
+    user_token = cursor.fetchone()
+
+    refresh_token = user_token['refresh_token']
 
     if not refresh_token:
         return jsonify({"error": "No refresh token available"}), 401
@@ -185,29 +202,78 @@ def get_new_access_token():
             "refresh_token": refresh_token,
             "client_id": client_id
         },
+        cert=("resources/fire.crt", "resources/fire.key"),
+        headers={"Content-Type": "application/x-www-form-urlencoded"}
     )
 
     if response.status_code == 200:
         token_data = response.json()
-        mySession['access_token'] = token_data['access_token']
-        mySession['access_token_expiry'] = time.time() + token_data['expires_in']
-        if 'refresh_token' in token_data:
-            mySession['refresh_token'] = token_data['refresh_token']
-        return token_data['access_token']
+        save_token(token_data['access_token'], token_data['refresh_token'], token_data['expires_in'], 'update')
+        return jsonify(token_data)
     else:
+        print("Error refreshing token:", response.text)
         return jsonify({"error": "Failed to refresh token"}), 401
 
 
 
 def get_access_token():
-    access_token = mySession.get('access_token')
-    expiry = mySession.get('access_token_expire', 0)
+    user_id = session.get('user_id')
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT user_tokens.access_token, user_tokens.token_expiry
+    FROM user_credentials
+    JOIN user_tokens ON user_credentials.id = user_tokens.user_id
+    WHERE user_credentials.id = %s
+    """, (user_id,))
 
-    if not access_token or time.time() > expiry:
+    user_token = cursor.fetchone()
+
+    token_expiry = user_token['token_expiry']
+    access_token = user_token['access_token']
+    current_time = datetime.fromtimestamp(time.time())
+
+    if not access_token or current_time > token_expiry:
+        print("bye")
         access_token = get_new_access_token()
 
     return access_token
 
+
+
+def save_token(access_token, refresh_token, token_expiry, mode):
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+
+    new_token_expiry = datetime.now() + timedelta(seconds=299)
+
+    user_id = session.get('user_id')
+
+    if (mode == 'insert'):
+        print("opo")
+        cursor.execute("""
+        INSERT INTO user_tokens (user_id, access_token, refresh_token, token_expiry) 
+        VALUES (%s, %s, %s, %s)
+        ON DUPLICATE KEY UPDATE 
+            access_token = VALUES(access_token),
+            refresh_token = VALUES(refresh_token),
+            token_expiry = VALUES(token_expiry),
+            updated_at = CURRENT_TIMESTAMP
+        """
+        , (user_id, access_token, refresh_token, new_token_expiry))
+        conn.commit()
+    elif (mode == 'update'):
+        print("pop")
+        cursor.execute("""
+        UPDATE user_tokens
+        SET 
+            access_token = %s,
+            refresh_token = %s,
+            token_expiry = %s
+        WHERE user_id = %s
+        """
+        , (access_token, refresh_token, new_token_expiry, user_id))
+        conn.commit()
 
 
 
@@ -216,7 +282,7 @@ def get_access_token():
 def get_db_connection():
     conn = mysql.connector.connect(
         host = 'localhost',
-        user = 'alex',
+        user = 'debian',
         password = 'password',
         database = 'fire_heureka_credentials'
     )
