61a067ba3cc06b64b71d363e417bef8a7aff4193
guardina <guardinialex@gmail.com>
2024-11-14 17:23:32 +0100

    Add update and revoke functions

--- app.py
+++ app.py
@@ -27,11 +27,6 @@ token_url = 'https://token.testing.heureka.health/oauth2/token'
 auth_url = 'https://portal.testing.heureka.health/authorization'
 configuration_url = 'https://api.testing.heureka.health/api-configuration'
 
-mySession = {}
-mySession['access_token'] = None
-mySession['refresh_token'] = None
-mySession['access_token_expire'] = 0
-
 
 
 ##############################################  WEBPAGES  ##############################################
@@ -52,7 +47,12 @@ def login():
         
         cursor.execute("SELECT * FROM user_credentials WHERE username = %s", (username,))
         user = cursor.fetchone()
-        cursor.execute("SELECT user_tokens.access_token FROM user_credentials JOIN user_tokens ON user_credentials.id = user_tokens.user_id WHERE user_credentials.username = %s", (username,))
+        cursor.execute("""
+        SELECT user_tokens.access_token
+        FROM user_credentials
+        JOIN user_tokens ON user_credentials.id = user_tokens.user_id
+        WHERE user_credentials.username = %s"""
+        , (username,))
         user_token = cursor.fetchone()
 
         if not user:
@@ -75,54 +75,65 @@ def login():
 
 @app.route('/callback', methods=['GET', 'POST'])
 def redirected_page():
-    auth_code = request.args.get('code')
-    if not auth_code:
-        return 'Authorization failed. No code provided.', 400
+    print(session['mode'])
+    if session['mode']:
+        if session['mode'] == 'authorize':
+            auth_code = request.args.get('code')
+            if not auth_code:
+                return 'Authorization failed. No code provided.', 400
 
-    redirect_uri = 'http://localhost:5000/callback'
+            redirect_uri = 'http://localhost:5000/callback'
 
-    payload = {
-        "grant_type": "authorization_code",
-        "client_id": client_id,
-        "redirect_uri": redirect_uri,
-        "code": auth_code
-    }
+            payload = {
+                "grant_type": "authorization_code",
+                "client_id": client_id,
+                "redirect_uri": redirect_uri,
+                "code": auth_code
+            }
 
 
-    try:
-        response = requests.post(
-            token_url,
-            data=payload,
-            cert=("resources/fire.crt", "resources/fire.key"),
-            headers={"Content-Type": "application/x-www-form-urlencoded"}
-        )
-        
-        if response.status_code == 200:
-            token_data = response.json()
-            access_token = token_data.get('access_token')
-            refresh_token = token_data.get('refresh_token')
-            installation_id = jwt.decode(access_token, options={"verify_signature": False}).get('sub')
-            expiration_time = token_data.get('expires_in')
+            try:
+                response = requests.post(
+                    token_url,
+                    data=payload,
+                    cert=("resources/fire.crt", "resources/fire.key"),
+                    headers={"Content-Type": "application/x-www-form-urlencoded"}
+                )
+                
+                if response.status_code == 200:
+                    token_data = response.json()
+                    access_token = token_data.get('access_token')
+                    refresh_token = token_data.get('refresh_token')
+                    installation_id = jwt.decode(access_token, options={"verify_signature": False}).get('sub')
+                    expiration_time = token_data.get('expires_in')
 
-            save_token(access_token, refresh_token, expiration_time, 'insert')
+                    save_token(access_token, refresh_token, expiration_time, 'insert')
 
-            conn = get_db_connection()
-            cursor = conn.cursor(dictionary=True)
+                    conn = get_db_connection()
+                    cursor = conn.cursor(dictionary=True)
 
-            user_id = session.get('user_id')
+                    user_id = session.get('user_id')
 
-            cursor.execute("""
-            UPDATE user_credentials SET installation_id = %s WHERE id = %s
-            """
-            , (installation_id, user_id))
-            conn.commit()
+                    cursor.execute("""
+                    UPDATE user_credentials SET installation_id = %s WHERE id = %s
+                    """
+                    , (installation_id, user_id))
+                    conn.commit()
 
-            return redirect(url_for('change_password'))
-        else:
-            return f"Failed to retrieve token. Status code: {response.status_code}, Error: {response.text}", response.status_code
+                    return redirect(url_for('change_password'))
+                else:
+                    return f"Failed to retrieve token. Status code: {response.status_code}, Error: {response.text}", response.status_code
 
-    except requests.exceptions.RequestException as e:
-        return f"An error occurred while requesting the token: {str(e)}", 500
+            except requests.exceptions.RequestException as e:
+                return f"An error occurred while requesting the token: {str(e)}", 500
+
+        elif session['mode'] == 'update':
+            print('updato')
+            return render_template('heureka_connection.html')
+
+        elif session['mode'] == 'revoke':
+            print('revoko')
+            return render_template('heureka_connection.html')
     
 
 @app.route('/change_password', methods=['GET', 'POST'])
@@ -171,6 +182,46 @@ def heureka_api():
     '''
 
 
+@app.route('/heureka_update', methods=['GET', 'POST'])
+def update_permissions():
+    user_id = session.get('user_id')
+    redirect_url = 'http://localhost:5000/callback'
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT installation_id
+    FROM user_credentials
+    WHERE id = %s
+    """
+    , (user_id,))
+
+    installation_id = cursor.fetchone()['installation_id']
+    session['mode'] = 'update'
+    url = f'{auth_url}/update?installation_id={installation_id}&redirect_uri={redirect_url}'
+   
+    return redirect(url)
+
+
+@app.route('/heureka_revoke', methods=['GET', 'POST'])
+def remove_permissions():
+    user_id = session.get('user_id')
+    redirect_url = 'http://localhost:5000/callback'
+
+    conn = get_db_connection()
+    cursor = conn.cursor(dictionary=True)
+    cursor.execute("""
+    SELECT installation_id
+    FROM user_credentials
+    WHERE id = %s
+    """
+    , (user_id,))
+
+    installation_id = cursor.fetchone()['installation_id']
+    session['mode'] = 'revoke'
+    url = f'{auth_url}/revoke?installation_id={installation_id}&redirect_uri={redirect_url}'
+
+    return redirect(url)
 
 
 ##############################################  TOKEN FUNCTIONS  ##############################################
@@ -234,7 +285,6 @@ def get_access_token():
     current_time = datetime.fromtimestamp(time.time())
 
     if not access_token or current_time > token_expiry:
-        print("bye")
         access_token = get_new_access_token()
 
     return access_token
@@ -250,7 +300,6 @@ def save_token(access_token, refresh_token, token_expiry, mode):
     user_id = session.get('user_id')
 
     if (mode == 'insert'):
-        print("opo")
         cursor.execute("""
         INSERT INTO user_tokens (user_id, access_token, refresh_token, token_expiry) 
         VALUES (%s, %s, %s, %s)
@@ -263,7 +312,6 @@ def save_token(access_token, refresh_token, token_expiry, mode):
         , (user_id, access_token, refresh_token, new_token_expiry))
         conn.commit()
     elif (mode == 'update'):
-        print("pop")
         cursor.execute("""
         UPDATE user_tokens
         SET 
@@ -293,6 +341,7 @@ def get_db_connection():
 def heureka_authorize():
     random_state = secrets.token_urlsafe(32)
     redirect_url = 'http://localhost:5000/callback'
+    session['mode'] = 'authorize'
     url = f'{auth_url}/grant?client_id={client_id}&state={random_state}&redirect_uri={redirect_url}'
 
     return redirect(url)
